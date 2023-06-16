b610e3ea3c8686d5c288f9472f4e503f7153c758
domenico2003 <domdat03@gmail.com>
2023-06-16 11:23:27 +0200

    setup progetto

---
+++ src/main/java/application/U5S3ProgettoSettimanaleDattiloApplication.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5S3ProgettoSettimanaleDattiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5S3ProgettoSettimanaleDattiloApplication.class, args);
+	}
+
+}
